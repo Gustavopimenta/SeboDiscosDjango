315ed3ea1d63a0b0a667bc5e3e00085c554eb0f7
Gustavo Pimenta <gustav.alves17@gmail.com>
2023-10-08 01:18:48 -0300

    parte 7 da atividade

--- SeboDiscos/discos/admin.py
+++ SeboDiscos/discos/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Disco
+from .models import Disco, SeloFonografico, Artista
 
 admin.site.register(Disco)
+admin.site.register(SeloFonografico)
+admin.site.register(Artista)
 
--- SeboDiscos/discos/models.py
+++ SeboDiscos/discos/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
+class SeloFonografico(models.Model):
+    nome = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nome
+
+class Artista(models.Model):
+    nome = models.CharField(max_length=255)
+    discos = models.ManyToManyField('Disco', related_name='artistas')
+    
+    def __str__(self):
+        return self.nome
+
 class Disco(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
-    selo_fonografico = models.CharField(max_length=255)
+    selo_fonografico = models.ForeignKey(SeloFonografico, on_delete=models.CASCADE)
     ano = models.IntegerField()
     pais = models.CharField(max_length=255)
     genero = models.CharField(max_length=255)
     quantidade = models.IntegerField()
-
+    
     def __str__(self):
         return self.nome
-
-
